29a1195aef490f6b9385c42080fd82fde2c3cef5
Yuuji <varian.khasira@gmail.com>
2024-07-29 11:48:23 +0700

    cp

--- user_validator.js
+++ user_validator.js
@@ -1,31 +1,39 @@
-import readlineSync from 'readline-sync';
-import validator from 'validator';
-import chalk from 'chalk';
-import validatorPonsel from 'validator-ponsel';
+import readlineSync from "readline-sync";
+import validator from "validator";
+import chalk from "chalk";
+import validatorPonsel from "validator-ponsel";
 
 // Function to ask user for input and validate email and phone
 async function getUserDetails() {
-    const name = readlineSync.question('Enter your name: ');
+    const name = readlineSync.question("Enter your name: ");
 
-    let email = readlineSync.question('Enter your email: ');
+    let email = readlineSync.question("Enter your email: ");
     while (!validator.isEmail(email)) {
-        console.log(chalk.red('Invalid email format. Please enter a valid email.'));
-        email = readlineSync.question('Enter your email: ');
+        console.log(
+            chalk.red("Invalid email format. Please enter a valid email.")
+        );
+        email = readlineSync.question("Enter your email: ");
     }
 
     // const { default: validatorPhone } = await import('validator-ponsel');
-    
-    let phone = readlineSync.question('Enter your phone number: ');
-    while (!validatorPonsel(phone, 'any')) {
-        console.log(chalk.blue('Invalid phone number. Please enter a valid phone number.'));
-        phone = readlineSync.question('Enter your phone number: ');
+
+    let phone = readlineSync.question("Enter your phone number: ");
+    while (!validatorPonsel(phone, "any")) {
+        console.log(
+            chalk.blue(
+                "Invalid phone number. Please enter a valid phone number."
+            )
+        );
+        phone = readlineSync.question("Enter your phone number: ");
     }
 
     return { name, email, phone };
 }
 
-getUserDetails().then(userDetails => {
-    console.log('User Details:', userDetails);
-}).catch(error => {
-    console.error('Error getting user details:', error);
-});
+getUserDetails()
+    .then((userDetails) => {
+        console.log("User Details:", userDetails);
+    })
+    .catch((error) => {
+        console.error("Error getting user details:", error);
+    });
